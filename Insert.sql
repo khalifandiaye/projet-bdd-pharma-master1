707831dc4363360435a16c25c293fd41258cffd9
baptiste.train <baptiste.train@gmail.com>
2012-12-07 15:03:06 +0000

    Les insert into.

---
+++ Insert.sql
@@ -0,0 +1,180 @@
+Antalgique - 
+
+Doliprane - directeur
+Pharmacie 
+
+INSERT INTO THESAURUS VALUES (1,"Français","Jean Genot","2012-12-06","Milieu Pharmaceutique",0,"Jean Genot","Informatisé","Dictionnaire","Jean Genot");
+
+INSERT INTO DOMAINE VALUES(1,"Pharmacie","Milieu Pharmaceutique",Thesaurus_T(1,"Français","Jean Genot","2012-12-06","Milieu Pharmaceutique",0,"Jean Genot","Informatisé","Dictionnaire","Jean Genot"));
+
+INSER INTO CONCEPT VALUES(1,"2012-12-06",
+	DOMAINE(1,"Pharmacie","Milieu Pharmaceutique",
+			Thesaurus_T(1,"Français","Jean Genot","2012-12-06","Milieu 				Pharmaceutique",0,"Jean 									Genot","Informatisé","Dictionnaire","Jean 			Genot")),Descripteur_T(1,"2012-12-06","Pharmacie","Jean Genot","Français"),1,1,1);
+
+INSERT INTO DESCRIPTEUR VALUES (1,"2012-12-06","Pharmacie","Jean Genot","Français");
+
+INSER INTO CONCEPT VALUES(2,"2012-12-06",
+	DOMAINE(1,"Pharmacie","Milieu Pharmaceutique",
+			Thesaurus_T(1,"Français","Jean Genot","2012-12-06","Milieu 				Pharmaceutique",0,"Jean 									Genot","Informatisé","Dictionnaire","Jean 			Genot")),Descripteur_T(2,"2012-12-06","Médicament","Jean Genot","Français"),2,2,2);
+
+INSERT INTO DESCRIPTEUR VALUES (2,"2012-12-06","Médicament","Jean Genot","Français");
+
+INSER INTO CONCEPT VALUES(3,"2012-12-06",
+	DOMAINE(1,"Pharmacie","Milieu Pharmaceutique",
+			Thesaurus_T(1,"Français","Jean Genot","2012-12-06","Milieu 				Pharmaceutique",0,"Jean 									Genot","Informatisé","Dictionnaire","Jean 			Genot")),Descripteur_T(3,"2012-12-06","Patient","Jean Genot","Français"),3,3,3);
+			
+INSERT INTO DESCRIPTEUR VALUES (3,"2012-12-06","Patient","Jean Genot","Français");
+
+INSER INTO CONCEPT VALUES(4,"2012-12-06",
+	DOMAINE(1,"Pharmacie","Milieu Pharmaceutique",
+			Thesaurus_T(1,"Français","Jean Genot","2012-12-06","Milieu 				Pharmaceutique",0,"Jean 									Genot","Informatisé","Dictionnaire","Jean 			Genot")),Descripteur_T(4,"2012-12-06","Laboratoire","Jean Genot","Français"),4,4,4);
+
+INSERT INTO DESCRIPTEUR VALUES (4,"2012-12-06","Laboratoire","Jean Genot","Français");
+
+INSER INTO CONCEPT VALUES(5,"2012-12-06",
+	DOMAINE(1,"Pharmacie","Milieu Pharmaceutique",
+			Thesaurus_T(1,"Français","Jean Genot","2012-12-06","Milieu 				Pharmaceutique",0,"Jean 									Genot","Informatisé","Dictionnaire","Jean 			Genot")),Descripteur_T(5,"2012-12-06","Grossiste","Jean Genot","Français"),5,5,5);
+
+INSERT INTO DESCRIPTEUR VALUES (5,"2012-12-06","Grossiste","Jean Genot","Français");
+
+INSER INTO CONCEPT VALUES(6,"2012-12-06",
+	DOMAINE(1,"Pharmacie","Milieu Pharmaceutique",
+			Thesaurus_T(1,"Français","Jean Genot","2012-12-06","Milieu 				Pharmaceutique",0,"Jean 									Genot","Informatisé","Dictionnaire","Jean 			Genot")),Descripteur_T(6,"2012-12-06","Vente","Jean Genot","Français"),6,6,6);
+
+INSERT INTO DESCRIPTEUR VALUES (6,"2012-12-06","Vente","Jean Genot","Français");
+
+INSER INTO CONCEPT VALUES(7,"2012-12-06",
+	DOMAINE(1,"Pharmacie","Milieu Pharmaceutique",
+			Thesaurus_T(1,"Français","Jean Genot","2012-12-06","Milieu 				Pharmaceutique",0,"Jean 									Genot","Informatisé","Dictionnaire","Jean 			Genot")),Descripteur_T(7,"2012-12-06","Commande","Jean Genot","Français"),7,7,7);
+
+INSERT INTO DESCRIPTEUR VALUES (7,"2012-12-06","Commande","Jean Genot","Français");
+
+INSER INTO CONCEPT VALUES(8,"2012-12-06",
+	DOMAINE(1,"Pharmacie","Milieu Pharmaceutique",
+			Thesaurus_T(1,"Français","Jean Genot","2012-12-06","Milieu 				Pharmaceutique",0,"Jean 									Genot","Informatisé","Dictionnaire","Jean 			Genot")),Descripteur_T(8,"2012-12-06","Dossier Pharmaceutique","Jean Genot","Français"),8,8,8);
+			
+INSERT INTO DESCRIPTEUR VALUES (8,"2012-12-06","Dossier Pharmaceutique","Jean Genot","Français");
+			
+INSER INTO CONCEPT VALUES(9,"2012-12-06",
+	DOMAINE(1,"Pharmacie","Milieu Pharmaceutique",
+			Thesaurus_T(1,"Français","Jean Genot","2012-12-06","Milieu 				Pharmaceutique",0,"Jean 									Genot","Informatisé","Dictionnaire","Jean 			Genot")),Descripteur_T(9,"2012-12-06","Achat","Jean Genot","Français"),9,9,9);
+			
+INSERT INTO DESCRIPTEUR VALUES (9,"2012-12-06","Achat","Jean Genot","Français");			
+			
+INSER INTO CONCEPT VALUES(10,"2012-12-06",
+	DOMAINE(1,"Pharmacie","Milieu Pharmaceutique",
+			Thesaurus_T(1,"Français","Jean Genot","2012-12-06","Milieu 				Pharmaceutique",0,"Jean 									Genot","Informatisé","Dictionnaire","Jean 			Genot")),Descripteur_T(10,"2012-12-06","Bon de Livraison","Jean Genot","Français"),10,10,10);
+
+INSERT INTO DESCRIPTEUR VALUES (10,"2012-12-06","Bon de Livraison","Jean Genot","Français");
+
+INSER INTO CONCEPT VALUES(11,"2012-12-06",
+	DOMAINE(1,"Pharmacie","Milieu Pharmaceutique",
+			Thesaurus_T(1,"Français","Jean Genot","2012-12-06","Milieu 				Pharmaceutique",0,"Jean 									Genot","Informatisé","Dictionnaire","Jean 			Genot")),Descripteur_T(11,"2012-12-06","Email","Jean Genot","Français"),11,11,11);
+			
+INSERT INTO DESCRIPTEUR VALUES (11,"2012-12-06","Email","Jean Genot","Français");			
+			
+INSER INTO CONCEPT VALUES(12,"2012-12-06",
+	DOMAINE(1,"Pharmacie","Milieu Pharmaceutique",
+			Thesaurus_T(1,"Français","Jean Genot","2012-12-06","Milieu 				Pharmaceutique",0,"Jean 									Genot","Informatisé","Dictionnaire","Jean 			Genot")),Descripteur_T(12,"2012-12-06","Fournisseur","Jean Genot","Français"),12,12,12);
+			
+INSERT INTO DESCRIPTEUR VALUES (12,"2012-12-06","Fournisseur","Jean Genot","Français");
+			
+INSER INTO CONCEPT VALUES(13,"2012-12-06",
+	DOMAINE(1,"Pharmacie","Milieu Pharmaceutique",
+			Thesaurus_T(1,"Français","Jean Genot","2012-12-06","Milieu 				Pharmaceutique",0,"Jean 									Genot","Informatisé","Dictionnaire","Jean 			Genot")),Descripteur_T(13,"2012-12-06","Haldol","Jean Genot","Français"),13,13,13);
+			
+INSERT INTO DESCRIPTEUR VALUES (13,"2012-12-06","haldol","Jean Genot","Français");			
+			
+INSER INTO CONCEPT VALUES(14,"2012-12-06",
+	DOMAINE(1,"Pharmacie","Milieu Pharmaceutique",
+			Thesaurus_T(1,"Français","Jean Genot","2012-12-06","Milieu 				Pharmaceutique",0,"Jean 									Genot","Informatisé","Dictionnaire","Jean 			Genot")),Descripteur_T(14,"2012-12-06","Ixprime","Jean Genot","Français"),14,14,14);
+			
+INSERT INTO DESCRIPTEUR VALUES (14,"2012-12-06","Ixprime","Jean Genot","Français");			
+			
+INSER INTO CONCEPT VALUES(15,"2012-12-06",
+	DOMAINE(1,"Pharmacie","Milieu Pharmaceutique",
+			Thesaurus_T(1,"Français","Jean Genot","2012-12-06","Milieu 				Pharmaceutique",0,"Jean 									Genot","Informatisé","Dictionnaire","Jean 			Genot")),Descripteur_T(15,"2012-12-06","Juvamine","Jean Genot","Français"),15,15,15);
+			
+INSERT INTO DESCRIPTEUR VALUES (15,"2012-12-06","Juvamine","Jean Genot","Français");			
+			
+INSER INTO CONCEPT VALUES(16,"2012-12-06",
+	DOMAINE(1,"Pharmacie","Milieu Pharmaceutique",
+			Thesaurus_T(1,"Français","Jean Genot","2012-12-06","Milieu 				Pharmaceutique",0,"Jean 									Genot","Informatisé","Dictionnaire","Jean 			Genot")),Descripteur_T(16,"2012-12-06","K Lyte","Jean Genot","Français"),16,16,16);
+			
+INSERT INTO DESCRIPTEUR VALUES (16,"2012-12-06","K Lyte","Jean Genot","Français");			
+			
+INSER INTO CONCEPT VALUES(17,"2012-12-06",
+	DOMAINE(1,"Pharmacie","Milieu Pharmaceutique",
+			Thesaurus_T(1,"Français","Jean Genot","2012-12-06","Milieu 				Pharmaceutique",0,"Jean 									Genot","Informatisé","Dictionnaire","Jean 			Genot")),Descripteur_T(17,"2012-12-06","Nom","Jean Genot","Français"),0,0,0);
+			
+INSERT INTO DESCRIPTEUR VALUES (17,"2012-12-06","Nom","Jean Genot","Français");			
+			
+INSER INTO CONCEPT VALUES(18,"2012-12-06",
+	DOMAINE(1,"Pharmacie","Milieu Pharmaceutique",
+			Thesaurus_T(1,"Français","Jean Genot","2012-12-06","Milieu 				Pharmaceutique",0,"Jean 									Genot","Informatisé","Dictionnaire","Jean 			Genot")),Descripteur_T(18,"2012-12-06","Ordonnance","Jean Genot","Français"),0,0,0);		
+				
+INSERT INTO DESCRIPTEUR VALUES (18,"2012-12-06","Ordonnance","Jean Genot","Français");
+
+INSER INTO CONCEPT VALUES(19,"2012-12-06",
+	DOMAINE(1,"Pharmacie","Milieu Pharmaceutique",
+			Thesaurus_T(1,"Français","Jean Genot","2012-12-06","Milieu 				Pharmaceutique",0,"Jean 									Genot","Informatisé","Dictionnaire","Jean 			Genot")),Descripteur_T(19,"2012-12-06","Quantite","Jean Genot","Français"),0,0,0);
+
+INSERT INTO DESCRIPTEUR VALUES (19,"2012-12-06","Quantite","Jean Genot","Français");
+
+INSER INTO CONCEPT VALUES(20,"2012-12-06",
+	DOMAINE(1,"Pharmacie","Milieu Pharmaceutique",
+			Thesaurus_T(1,"Français","Jean Genot","2012-12-06","Milieu 				Pharmaceutique",0,"Jean 									Genot","Informatisé","Dictionnaire","Jean 			Genot")),Descripteur_T(20,"2012-12-06","Remeron","Jean Genot","Français"),0,0,0);
+	
+INSERT INTO DESCRIPTEUR VALUES (20,"2012-12-06","Remeron","Jean Genot","Français");	
+	
+INSER INTO CONCEPT VALUES(21,"2012-12-06",
+	DOMAINE(1,"Pharmacie","Milieu Pharmaceutique",
+			Thesaurus_T(1,"Français","Jean Genot","2012-12-06","Milieu 				Pharmaceutique",0,"Jean 									Genot","Informatisé","Dictionnaire","Jean 			Genot")),Descripteur_T(21,"2012-12-06","Salle","Jean Genot","Français"),0,0,0);
+
+INSERT INTO DESCRIPTEUR VALUES (21,"2012-12-06","Salle","Jean Genot","Français");
+
+INSER INTO CONCEPT VALUES(22,"2012-12-06",
+	DOMAINE(1,"Pharmacie","Milieu Pharmaceutique",
+			Thesaurus_T(1,"Français","Jean Genot","2012-12-06","Milieu 				Pharmaceutique",0,"Jean 									Genot","Informatisé","Dictionnaire","Jean 			Genot")),Descripteur_T(22,"2012-12-06","Téléphone","Jean Genot","Français"),0,0,0);
+
+INSERT INTO DESCRIPTEUR VALUES (22,"2012-12-06","Télépĥone","Jean Genot","Français");
+
+INSER INTO CONCEPT VALUES(23,"2012-12-06",
+	DOMAINE(1,"Pharmacie","Milieu Pharmaceutique",
+			Thesaurus_T(1,"Français","Jean Genot","2012-12-06","Milieu 				Pharmaceutique",0,"Jean 									Genot","Informatisé","Dictionnaire","Jean 			Genot")),Descripteur_T(23,"2012-12-06","Urbanyl","Jean Genot","Français"),0,0,0);
+			
+INSERT INTO DESCRIPTEUR VALUES (23,"2012-12-06","Urbanyl","Jean Genot","Français");			
+			
+INSER INTO CONCEPT VALUES(24,"2012-12-06",
+	DOMAINE(1,"Pharmacie","Milieu Pharmaceutique",
+			Thesaurus_T(1,"Français","Jean Genot","2012-12-06","Milieu 				Pharmaceutique",0,"Jean 									Genot","Informatisé","Dictionnaire","Jean 			Genot")),Descripteur_T(24,"2012-12-06","Wellvone","Jean Genot","Français"),0,0,0);
+			
+INSERT INTO DESCRIPTEUR VALUES (24,"2012-12-06","Wellvone","Jean Genot","Français");			
+			
+INSER INTO CONCEPT VALUES(25,"2012-12-06",
+	DOMAINE(1,"Pharmacie","Milieu Pharmaceutique",
+			Thesaurus_T(1,"Français","Jean Genot","2012-12-06","Milieu 				Pharmaceutique",0,"Jean 									Genot","Informatisé","Dictionnaire","Jean 			Genot")),Descripteur_T(25,"2012-12-06","Xanax","Jean Genot","Français"),0,0,0);
+			
+INSERT INTO DESCRIPTEUR VALUES (25,"2012-12-06","Xanax","Jean Genot","Français");			
+			
+INSER INTO CONCEPT VALUES(26,"2012-12-06",
+	DOMAINE(1,"Pharmacie","Milieu Pharmaceutique",
+			Thesaurus_T(1,"Français","Jean Genot","2012-12-06","Milieu 				Pharmaceutique",0,"Jean 									Genot","Informatisé","Dictionnaire","Jean 			Genot")),Descripteur_T(26,"2012-12-06","Yaz","Jean Genot","Français"),0,0,0);
+			
+INSERT INTO DESCRIPTEUR VALUES (26,"2012-12-06","Yaz","Jean Genot","Français");			
+			
+INSER INTO CONCEPT VALUES(27,"2012-12-06",
+	DOMAINE(1,"Pharmacie","Milieu Pharmaceutique",
+			Thesaurus_T(1,"Français","Jean Genot","2012-12-06","Milieu 				Pharmaceutique",0,"Jean 									Genot","Informatisé","Dictionnaire","Jean 			Genot")),Descripteur_T(27,"2012-12-06","Zantac","Jean Genot","Français"),0,0,0);
+			
+INSERT INTO DESCRIPTEUR VALUES (27,"2012-12-06","Zantac","Jean Genot","Français");			
+			
+INSERT INTO Concept_G VALUE(1,Groupe_Concept_T());
+INSERT INTO Concept_S VALUE(1,Groupe_Concept_T());
+INSERT INTO Concept_A VALUE(1,Groupe_Concept_T(
+			Concept_T(2,"2012-12-06",
+					DOMAINE(1,"Pharmacie","Milieu Pharmaceutique",
+					Thesaurus_T(1,"Français","Jean Genot","2012-12-06","Milieu 				Pharmaceutique",0,"Jean 									Genot","Informatisé","Dictionnaire","Jean 			Genot")),Descripteur_T(2,"2012-12-06","Médicament","Jean Genot","Français"),2,2,2),
+			Concept_T(3,"2012-12-06",
+					DOMAINE(1,"Pharmacie","Milieu Pharmaceutique",
+					Thesaurus_T(1,"Français","Jean Genot","2012-12-06","Milieu 				Pharmaceutique",0,"Jean 									Genot","Informatisé","Dictionnaire","Jean 			Genot")),Descripteur_T(3,"2012-12-06","Patient","Jean Genot","Français"),3,3,3)));
